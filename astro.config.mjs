2b6340ce3e37df2cb6b02c914c4b70770dd866df
henryemdth <henry.emdth@gmail.com>
2026-06-13 21:11:00 -0400

    chore: migrate lockfile from npm to pnpm

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,12 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 
-export default defineConfig({});
+const isGitHubActions = process.env.GITHUB_ACTIONS === 'true';
+
+const repoName = process.env.GITHUB_REPOSITORY ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}` : '';
+
+export default defineConfig({
+  site: isGitHubActions 
+    ? `https://${process.env.GITHUB_REPOSITORY_OWNER}.github.io` 
+    : 'http://localhost:4321',
+  base: isGitHubActions ? repoName : '/',
+});
